0ed2597f97867ad111839fb15ecef01cfcd4eb38
Максим Голяков <zigzariz@mail.ru>
2021-07-21 17:32:39 +0300

    Add Joke component view

--- components/Joke.vue
+++ components/Joke.vue
@@ -1,8 +1,44 @@
-<template></template>
+<template>
+  <div :class="liked ? 'joke joke_liked' : 'joke'">
+    <div class="joke__text">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eius
+        omnis vel quos ut! Commodi rerum quibusdam quis dolores laudantium
+        ducimus aperiam delectus corporis doloremque, unde animi ullam libero
+        eos sint! Quas, error accusamus! Dolorem inventore itaque dolorum vitae
+        deserunt reprehenderit ducimus cupiditate nam libero, labore corporis
+        quidem maxime ullam.
+      </p>
+    </div>
+    <joke-button class="joke__btn"></joke-button>
+  </div>
+</template>
 
 <script>
-export default {}
+export default {
+  props: {
+    liked: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
+.joke {
+  padding: 1em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.joke_liked {
+  background-color: lightgreen;
+}
+
+.joke__text {
+  min-width: 80%;
+}
 </style>
