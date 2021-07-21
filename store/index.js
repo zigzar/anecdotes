60103aeca9934202cd8ee7606b12c1d7eedb2dd7
Максим Голяков <zigzariz@mail.ru>
2021-07-21 18:18:33 +0300

    Add basic functions in store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,32 @@
 import axios from 'axios'
 
-export const state = () => ({})
+export const state = () => ({
+  jokes: [],
+  jokesLimit: 10,
+})
 
-export const getters = {}
+export const getters = {
+  getJokes() {
+    return state.jokes
+  },
+}
 
-export const mutations = {}
+export const mutations = {
+  setJokes(state, payload) {
+    state.jokes = payload
+  },
+}
 
-export const actions = {}
+export const actions = {
+  async fetch({ state, commit }) {
+    let response = await axios.get(
+      `https://v2.jokeapi.dev/joke/Any?type=single&amount=${state.jokesLimit}`
+    )
+    let data = response.data
+    if (data.error) {
+      alert('Sry, we have some troubles with API')
+      return
+    }
+    commit('setJokes', data.jokes)
+  },
+}
