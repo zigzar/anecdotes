d5914c3dd512d853277cfbf543ec14e1d9f3ad46
Максим Голяков <zigzariz@mail.ru>
2021-07-21 23:15:46 +0300

    Add like saving

--- store/index.js
+++ store/index.js
@@ -31,18 +31,20 @@ export const mutations = {
   updateLikedJokes(state, likedJokes) {
     state.likedJokes = likedJokes
     localStorage.setItem('likedJokes', JSON.stringify(likedJokes))
+    console.log(likedJokes)
   },
   setLikedJokes(state) {
-    state.likedJokes = JSON.parse(localStorage.getItem('likedJokes'))
+    let arr = JSON.parse(localStorage.getItem('likedJokes'))
+    if (arr) state.likedJokes = arr
   },
 }
 
 export const actions = {
-  async fetch({ state, commit }) {
+  async fetch({ state, commit, getters }) {
     let response
     try {
       response = await axios.get(
-        `https://v2.jokeapi.dev/joke/Any?type=single&amount=${state.jokesLimit}`
+        ` https://v2.jokeapi.dev/joke/Any?type=single&amount=${state.jokesLimit}`
       )
     } catch (error) {
       console.log(`Ошибка при попытке получить шутки. Описание: ${error}`)
@@ -52,19 +54,25 @@ export const actions = {
       alert('Sry, we have some troubles with API')
       return
     }
+    await commit('setLikedJokes')
+    let likedJokes = await this.getters.getLikedJokes
+    for (let i = 0; i < data.jokes.length; i++) {
+      if (likedJokes.find((item) => item.id == data.jokes[i].id))
+        data.jokes[i].liked = true
+    }
     await commit('setJokes', data.jokes)
   },
   async addLikedJoke({ getters, commit }, joke) {
-    let arr = this.getters.getLikedJokes
+    let arr = getters.getLikedJokes
     arr.push(joke)
-    await this.commit('updateLikedJokes', arr)
+    await commit('updateLikedJokes', arr)
   },
   async removeLikedJoke({ getters, commit }, joke) {
-    let arr = this.getters.getLikedJokes
+    let arr = getters.getLikedJokes
     let index = arr.findIndex((item) => item.id == joke.id)
     if (index != -1) {
       arr.splice(index, 1)
-      await this.commit('updateLikedJokes', arr)
+      await commit('updateLikedJokes', arr)
     }
   },
 }
